34e856a4411856465a62680b728fc26e9e7aa9df
donghyun <hyunee721@gmail.com>
2024-02-17 23:15:00 +0900

    Create Base Branch

--- main.js
+++
@@ -1,7 +0,0 @@
-(function (args) {
-    var temp = 1;
-    var str = 'apple';
-    console.log("Debug...".concat(temp));
-    var el = document.querySelector("#test");
-    el.innerHTML = '123123';
-})();
--- main.ts
+++ main.ts
@@ -1,12 +1,3 @@
-((args) => {
-
-    let temp : Number = 1;
-    let str : string = 'apple';
-
-    console.log(`Debug...${temp}`);
-
-    let el = <HTMLDivElement> document.querySelector("#test");
-
-    el.innerHTML = '123123';
-
-})();
+(()=> {
+    console.log(`Test Load...`);
+})()
